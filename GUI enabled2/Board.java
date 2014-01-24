b91de1602810544fe85def95222680eb423e438e
Joey--Zeng <jzengstuy@gmail.com>
2014-01-23 22:33:18 -0500

    buttons
    asd

--- GUI enabled2/Board.java
+++ GUI enabled2/Board.java
@@ -20,7 +20,7 @@ public class Board extends JFrame{
     private static ArrayList<Player> players;
     public static ImageIcon img;
     private static int i; 
-
+ 
 
     public Board(){
 	players = new ArrayList<Player>();
@@ -463,7 +463,7 @@ public class Board extends JFrame{
 				_roll.setToolTipText("Rolls the dice.");
 				_roll.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent event) {
-						rooll();
+					    rooll();
 					}
 				});
 
@@ -473,7 +473,7 @@ public class Board extends JFrame{
 				endTurn.setToolTipText("Ends your turn.");
 				endTurn.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent event) {
-					    //end();
+					    endTurnn();
 					}
 				});
 
@@ -483,19 +483,19 @@ public class Board extends JFrame{
 				deMort.setToolTipText("Demortgages a property.");
 				deMort.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent event) {
-					    //deMort();
+					    deMort();
 					}
 				});
 				
-				final JButton buyProp = new JButton("Buy Property");
+				final JButton buyProp = new JButton("Buy House");
 				buyProp.setVisible(true);
 				buyProp.setEnabled(false);
-				buyProp.setToolTipText("Buys a property.");
+				buyProp.setToolTipText("Builds on your property.");
 				buyProp.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent event) {
-					    //buyProperty();
+					    buyHouse();
 					}
-				});
+				    });
 				
 				//create roll button
 				final JButton newGame = new JButton("Start Game!");
@@ -590,180 +590,207 @@ public class Board extends JFrame{
 
 
     public static void rooll(){
-	listModel.set(2, players.get(0) + " : $" + players.get(0).getMoney());
-		listModel.set(3, "Property Owned: " + players.get(0).getPropertyOwned());
-		listModel.set(4, players.get(1) + " : $" + players.get(1).getMoney());
-		listModel.set(5, "Property Owned: " + players.get(1).getPropertyOwned());
-		action.append("\n\n" + players.get(i).getName()+"'s Turn");
-		Player ref = players.get(i);
+	listModel.set(2, players.get(0).getName() + " : $" + players.get(0).getMoney());
+	listModel.set(3, "Property Owned: " + players.get(0).getPropertyOwned());
+	listModel.set(4, players.get(1).getName() + " : $" + players.get(1).getMoney());
+	listModel.set(5, "Property Owned: " + players.get(1).getPropertyOwned());
+	Player ref = players.get(i);
+	
+	if (ref.getInJail()){
+	    action.append("\nJailed");
+	    JOptionPane.showMessageDialog(roll, ref.getName() + " is jailed.");
+	    ref.jailTurn();
+	    if(ref.getTurnsInJail() >= 2){
+		ref.jailBreak();
+	    }
+	    else if (ref.getCard()){ 
+		ref.loseCard();
+		ref.jailBreak();
+		action.append("Used card to get out of jail");
+	    }
+	    else{			
+		roll();
+		if(doubs()){
+		    ref.jailBreak();
+		    action.append("Snake Eyes! You are out of jail. You may move next turn.");
+		    JOptionPane.showMessageDialog(roll, "Snake Eyes! You are out of jail. You may move next turn.");
+			}
+		else if (ref.getMoney() >= 50){
+		    int ans = JOptionPane.showConfirmDialog(roll, "Bail for 50?", "Bail", JOptionPane.YES_NO_OPTION);
+		    if (ans == JOptionPane.YES_OPTION){
+			ref.loseMoney(50);
+			ref.jailBreak();
+		    }
+		}
+	    }
+	}// end inJail block
+	else{ // not in jail / free to move...
+	    doubCount = 0;
+	    for (int dum = 1; dum == 1; dum ++){
+		buttons.get(ref.getPos()).setIcon(null);
+		buttons.get(ref.getPos()).setHorizontalTextPosition(SwingConstants.CENTER);
+		ref.posAdd(roll());
 		
-	        if (ref.getInJail()){
-		    action.append("\nJailed");
-			JOptionPane.showMessageDialog(roll, ref.getName() + " is jailed.");
-		    ref.jailTurn();
-		    if(ref.getTurnsInJail() >= 2){
-			ref.jailBreak();
-		    }
-		    else if (ref.getCard()){ 
-			ref.loseCard();
-			ref.jailBreak();
-			action.append("Used card to get out of jail");
-		    }
-		    else{			
-			roll();
-			if(doubs()){
-			    ref.jailBreak();
-			    action.append("Snake Eyes! You are out of jail. You may move next turn.");
-				JOptionPane.showMessageDialog(roll, "Snake Eyes! You are out of jail. You may move next turn.");
-			}
-			else if (ref.getMoney() >= 50){
-			    int ans = JOptionPane.showConfirmDialog(roll, "Bail for 50?", "Bail", JOptionPane.YES_NO_OPTION);
-			    if (ans == JOptionPane.YES_OPTION){
-					ref.loseMoney(50);
-					ref.jailBreak();
-			    }
-			}
-		    }
-		}// end inJail block
-		else{ // not in jail / free to move...
-		    doubCount = 0;
-		    for (int dum = 1; dum == 1; dum ++){
-				buttons.get(ref.getPos()).setIcon(null);
-				buttons.get(ref.getPos()).setHorizontalTextPosition(SwingConstants.CENTER);
-				ref.posAdd(roll());
-				
-				if (i == 0){
-					img = new ImageIcon("p1.jpg");
-				}
-
-				else{
-					img = new ImageIcon("p2.gif");
-				}
-
-				buttons.get(ref.getPos()).setIcon(img);
-				buttons.get(ref.getPos()).setHorizontalTextPosition(SwingConstants.CENTER);
-								
-				
-			int abc = ref.propertyInteract(board.get(ref.getPos()), players, dice1, dice2);
-			ref.propertyInteract(board.get(abc), players, dice1, dice2);	
-			buttons.get(ref.getPos()).setIcon(null);
-			buttons.get(ref.getPos()).setHorizontalTextPosition(SwingConstants.CENTER);
-			buttons.get(ref.getPos()).setIcon(img);
-			buttons.get(ref.getPos()).setHorizontalTextPosition(SwingConstants.CENTER);
-			if (dice1 == dice2){
-			    if (doubCount >= 2){
-				ref.jailed();
-				break;
-			    }
-			    else{
-				doubCount ++;
-				dum--;
-				JOptionPane.showMessageDialog(roll, "Doubles! Roll again.");
-			    }
-			}
-			
-			if (ref.getMoney() < 0){
-			    if (!emergencyMort(ref)){
-			        removeP(ref); // REPLACE THIS WITH METHOD TO FREE ALL PROPS...
-			    }
-			}
-			if (players.size() <= 1){
-			    JOptionPane.showMessageDialog(roll, "Game is over. The remaining player has won.");
-			    break;
-			}					      
-		    }// end roll and interact (loops if double)
-		    String yesno = "y";
-		    while (yesno.equals("y")){
-			if (yesno.equals("y")){
-			    String options = "Color? ";
-			    if (ref.checkBrown()){
-				options += " 1)Brown ";
-			    }
-			    if (ref.checkLBlue()){
-				options += " 2)Light Blue ";
-			    }
-			    if (ref.checkPink()){
-				options += " 3)Pink ";
-			    }
-			    if (ref.checkOrange()){
-				options += " 4)Orange ";
-			    }
-			    if (ref.checkRed()){
-				options += " 5)Red ";
-			    }
-			    if (ref.checkYellow()){
-				options += " 6)Yellow ";
-			    }
-			    if (ref.checkGreen()){
-				options += " 7)Green ";
-			    }
-			    if (ref.checkBlue()){
-			        options += " 8)Blue ";
-			    }
-			    if (options.equals("Color? ")){
-			        yesno = "n";
-			    }
-			    else {
-				int ans = JOptionPane.showConfirmDialog(roll, "Do you want to purchase houses?", "Purchase House?", JOptionPane.YES_NO_OPTION);
-			    if (ans == JOptionPane.YES_OPTION){ 		
-					int color = Integer.parseInt( JOptionPane.showInputDialog(roll, options) );  // STILL NEED TO GUI THIS.... Just make a text box
-				int prop = 0;
-				if (color == 1) {
-				    prop = Integer.parseInt( JOptionPane.showInputDialog(roll, "Which property? 1)Mediterranean Ave  3)Baltic Ave") );
-				}
-				else if (color == 2) {
-				    prop = Integer.parseInt( JOptionPane.showInputDialog(roll, "Which property? 6)Oriental Ave  7)Vermont Ave  8)Connecticut Ave"));
-				}
-				else if (color == 3) {
-				    prop = Integer.parseInt( JOptionPane.showInputDialog(roll, "Which property? 11)St.Charles Place  13)States Ave  14)Virginia Ave"));
-				}
-				else if (color == 4) {
-				    prop = Integer.parseInt( JOptionPane.showInputDialog(roll, "Which property? 16)St.James Place  18)Tennessee Ave  19)NY Ave"));
-				}
-				else if (color == 5) {
-				    prop = Integer.parseInt( JOptionPane.showInputDialog(roll, "Which property? 21)Kentucky Ace  23)Indiana Ave  24)Illinois Ave"));
-				}
-				else if (color == 6) {
-				    prop = Integer.parseInt( JOptionPane.showInputDialog(roll, "Which property? 26)Atlantic Ave  27)Vermont Ave  29)Marvin Gardens"));
-				}
-				else if (color == 7) {
-				    prop = Integer.parseInt( JOptionPane.showInputDialog(roll, "Which property? 31)Pacific Ave  32)North Carolina Ave  34)Pennsylvania Ave"));
-				}
-				else if (color == 8) {
-				    prop = Integer.parseInt( JOptionPane.showInputDialog(roll, "Which property? 37)Park Place  39)Broadwalk"));
-				}	
-				int sub = board.get(prop).getAddOn();
-				if (sub == 5){
-				    JOptionPane.showMessageDialog(null, "Property is maxed already");
-				}
-				else{
-				    String prompt = "1)1 House  2)2 Houses 3)3 Houses 4)4 Houses 5)A Hotel   ";
-				    prompt = prompt.substring(sub*11);
-
-				    int buildings = ref.getMoney() / board.get(prop).getHouseCost();
-				    if (buildings == 0){
-					prompt = "You don't have the money to build here";
-				    }
-				    if(buildings >= 5){}
-				    else{
-					prompt = prompt.substring(0, prompt.length()-11*(5-buildings));
-				    }
-				    int _how = Integer.parseInt(JOptionPane.showInputDialog(null, prompt));
-				    ref.buyHouse(board.get(prop), _how);
-				}
-			    }		       			
-			    }// end if input = yes
-			}// ends while loop for houses
-			
-		    }// end not-in-jail block 
-		}// end loop block for one person's turn
 		if (i == 0){
-		    i = players.size()-1;
+		    img = new ImageIcon("p1.jpg");
 		}
-		else{ 
-		    i--;
+		
+		else{
+		    img = new ImageIcon("p2.gif");
 		}
+		
+		buttons.get(ref.getPos()).setIcon(img);
+		buttons.get(ref.getPos()).setHorizontalTextPosition(SwingConstants.CENTER);
+		
+		
+		int abc = ref.propertyInteract(board.get(ref.getPos()), players, dice1, dice2);
+		ref.propertyInteract(board.get(abc), players, dice1, dice2);	
+		buttons.get(ref.getPos()).setIcon(null);
+		buttons.get(ref.getPos()).setHorizontalTextPosition(SwingConstants.CENTER);
+		buttons.get(ref.getPos()).setIcon(img);
+		buttons.get(ref.getPos()).setHorizontalTextPosition(SwingConstants.CENTER);
+		listModel.set(2, players.get(0).getName() + " : $" + players.get(0).getMoney());
+		listModel.set(3, "Property Owned: " + players.get(0).getPropertyOwned());
+		listModel.set(4, players.get(1).getName() + " : $" + players.get(1).getMoney());
+		listModel.set(5, "Property Owned: " + players.get(1).getPropertyOwned());
+
+		if (dice1 == dice2){
+		    if (doubCount >= 2){
+			ref.jailed();
+			break;
+		    }
+		    else{
+			doubCount ++;
+			dum--;
+			JOptionPane.showMessageDialog(roll, "Doubles! Roll again.");
+		    }
+		}
+		
+		if (ref.getMoney() < 0){
+		    if (!emergencyMort(ref)){
+			removeP(ref); // REPLACE THIS WITH METHOD TO FREE ALL PROPS...
+		    }
+		}
+		if (players.size() <= 1){
+		    JOptionPane.showMessageDialog(roll, "Game is over. The remaining player has won.");
+		    break;
+		}					      
+	    }// end roll and interact (loops if double)
+	}
+	
+
+	
+    }// end rooll()
+    
+    public static void buyHouse(){
+	Player ref = players.get(i);
+	String options = "Color? ";
+	if (ref.checkBrown()){
+	    options += " 1)Brown ";
+	}
+	if (ref.checkLBlue()){
+	    options += " 2)Light Blue ";
+	}
+	if (ref.checkPink()){
+	    options += " 3)Pink ";
+	}
+	if (ref.checkOrange()){
+	    options += " 4)Orange ";
+	}
+	if (ref.checkRed()){
+	    options += " 5)Red ";
+	}
+	if (ref.checkYellow()){
+	    options += " 6)Yellow ";
+	}
+	if (ref.checkGreen()){
+	    options += " 7)Green ";
+	}
+	if (ref.checkBlue()){
+	    options += " 8)Blue ";
+		}
+	if (options.equals("Color? ")){
+	    options = "You have no full sets";
+		}		
+	int color = Integer.parseInt( JOptionPane.showInputDialog(roll, options) );  // STILL NEED TO GUI THIS.... Just make a text box
+	int prop = 0;
+	if (color == 1) {
+	    prop = Integer.parseInt( JOptionPane.showInputDialog(roll, "Which property? 1)Mediterranean Ave  3)Baltic Ave") );
+	}
+	else if (color == 2) {
+	    prop = Integer.parseInt( JOptionPane.showInputDialog(roll, "Which property? 6)Oriental Ave  7)Vermont Ave  8)Connecticut Ave"));
+	}
+	else if (color == 3) {
+	    prop = Integer.parseInt( JOptionPane.showInputDialog(roll, "Which property? 11)St.Charles Place  13)States Ave  14)Virginia Ave"));
+	}
+	else if (color == 4) {
+	    prop = Integer.parseInt( JOptionPane.showInputDialog(roll, "Which property? 16)St.James Place  18)Tennessee Ave  19)NY Ave"));
+	}
+	else if (color == 5) {
+	    prop = Integer.parseInt( JOptionPane.showInputDialog(roll, "Which property? 21)Kentucky Ace  23)Indiana Ave  24)Illinois Ave"));
+	}
+	else if (color == 6) {
+	    prop = Integer.parseInt( JOptionPane.showInputDialog(roll, "Which property? 26)Atlantic Ave  27)Vermont Ave  29)Marvin Gardens"));
+	}
+	else if (color == 7) {
+	    prop = Integer.parseInt( JOptionPane.showInputDialog(roll, "Which property? 31)Pacific Ave  32)North Carolina Ave  34)Pennsylvania Ave"));
+	}
+	else if (color == 8) {
+	    prop = Integer.parseInt( JOptionPane.showInputDialog(roll, "Which property? 37)Park Place  39)Broadwalk"));
+	}	
+	int sub = board.get(prop).getAddOn();
+	if (sub == 5){
+	    JOptionPane.showMessageDialog(null, "Property is maxed already");
+	}
+	else{
+	    String prompt = "1)1 House  2)2 Houses 3)3 Houses 4)4 Houses 5)A Hotel   ";
+	    prompt = prompt.substring(sub*11);
+	    
+	    int buildings = ref.getMoney() / board.get(prop).getHouseCost();
+	    if (buildings == 0){
+		prompt = "You don't have the money to build here";
+		JOptionPane.showMessageDialog(null, prompt);
+	    }
+	    else{
+		if(buildings >= 5){}
+		else{
+		    prompt = prompt.substring(0, prompt.length()-11*(5-buildings));
+		}
+		int _how = Integer.parseInt(JOptionPane.showInputDialog(null, prompt));
+		ref.buyHouse(board.get(prop), _how);
+	    }
+	}
+
+    }// end buyHouse()		
+
+
+    public static void endTurnn(){
+	Player ref = players.get(i);
+	if (ref.getMoney() < 0){
+	    if (!emergencyMort(ref)){
+		removeP(ref); // REPLACE THIS WITH METHOD TO FREE ALL PROPS...
+	    }
+	}
+	if (i == players.size()-1){
+	    i = 0;
+	}
+	else{ 
+	    i++;
+	}	
+	action.append("\n\n" + players.get(i).getName()+"'s Turn");	
     }
+
+    public static void deMort(){
+	Player ref = players.get(i);
+	if (!ref.getMortgage().isEmpty()){
+	    int num = Integer.parseInt(JOptionPane.showInputDialog(roll, propPrint(ref.getMortgage())));
+	    ref.deMort(board.get(num), ref.getMortgage().indexOf(num));
+	}
+	else
+	    JOptionPane.showMessageDialog(null, "You don't have mortgaged properties");
+
+    }
+		
 
     public static void updateL(){}
 	
@@ -776,7 +803,13 @@ public class Board extends JFrame{
 
 	players.add(new Player(one));
 	players.add(new Player(two));
-	int i = players.size()-1;
+	int i = 0;
+	listModel.set(2, players.get(0).getName() + " : $" + players.get(0).getMoney());
+	listModel.set(3, "Property Owned: " + players.get(0).getPropertyOwned());
+	listModel.set(4, players.get(1).getName() + " : $" + players.get(1).getMoney());
+	listModel.set(5, "Property Owned: " + players.get(1).getPropertyOwned());
+	action.append("\n\n" + players.get(i).getName()+"'s Turn");
+    }
 
 	/*
 	while ( players.size() > 1 ) {
@@ -946,7 +979,6 @@ public class Board extends JFrame{
 	}// end while loop block for one round of turns
 	}
 	*/
-    }
     
 
 
